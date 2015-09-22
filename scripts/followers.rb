622121cb318667e7a0527276f8648cdd2f851241
Austin Tai <austint@ocf.berkeley.edu>
2015-09-22 11:20:17 -0700

    wrong params in followers

--- scripts/followers.rb
+++ scripts/followers.rb
@@ -13,7 +13,7 @@ class Dekkai
 
   alias parse_received_event_followers parse_received_event
   def parse_received_event(event)
-    parse_received_event_followers
+    parse_received_event_followers(event)
     follow_user(event.source) if event.name == :follow
   end
 
